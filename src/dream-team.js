21fa117b6e34e35ef0d6340be85292fde1d89d07
Gabriellji <gabriellji@yahoo.com>
2020-03-22 17:00:48 +0300

    feat: done dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  let names = [];
+  for (let i = 0 ; i < members.length ; i++){
+    if (typeof members[i] == 'string') {
+      members[i] = members[i].split(' ').join('');
+      names.push(members[i][0]);
+    }
+  } 
+  for (let i = 0 ; i < names.length ; i++) {
+    if (names.length === 0) {
+      return false;
+    }
+      names[i] = names[i].toUpperCase();
+  }
+  return names.sort().join('');
+   
+  };
+
